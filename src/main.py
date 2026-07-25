4473dd776a17537ea65b79a1adc8b8d4f9ebfef1
Alinne de Souza Santos Castro <alinnecastro133@gmail.com>
2026-07-25 15:42:25 +0000

    Cenario 2

--- src/main.py
+++ src/main.py
@@ -1,29 +1,63 @@
 from machine import ADC, Pin
-from time import sleep_ms
+from time import sleep_ms, ticks_ms, ticks_diff
+
 
 # Configuração do LDR
 ldr = ADC(Pin(34))
 ldr.atten(ADC.ATTN_11DB)
 
+
+# Limites observados no Wokwi:
+# 800 lux gera aproximadamente 773 no ADC
+# 50 lux gera aproximadamente 2531 no ADC
 LIMITE_BLOQUEADO = 2000
 LIMITE_LIVRE = 1000
+
+# Tempo necessário para considerar micro-parada
+TEMPO_MICRO_PARADA_MS = 5000
+
 
 contador = 0
 sensor_bloqueado = False
 
+tempo_inicio_bloqueio = 0
+alerta_emitido = False
+
+
 print("Contador de Producao Inicializado")
+
 
 while True:
     valor = ldr.read()
 
-    # Peça bloqueando a luz
+    # A peça entrou e bloqueou a luz
     if valor > LIMITE_BLOQUEADO:
-        sensor_bloqueado = True
 
-    # Peça saiu e a luz voltou
+        # Executa somente no momento em que o bloqueio começa
+        if not sensor_bloqueado:
+            sensor_bloqueado = True
+            tempo_inicio_bloqueio = ticks_ms()
+            alerta_emitido = False
+
+        # Verifica se a peça ficou parada por 5 segundos
+        tempo_bloqueado = ticks_diff(
+            ticks_ms(),
+            tempo_inicio_bloqueio
+        )
+
+        if (
+            tempo_bloqueado >= TEMPO_MICRO_PARADA_MS
+            and not alerta_emitido
+        ):
+            print("Alerta: Micro-parada detectada!")
+            alerta_emitido = True
+
+    # A peça saiu e a luz voltou
     elif valor < LIMITE_LIVRE and sensor_bloqueado:
         contador += 1
         sensor_bloqueado = False
+        alerta_emitido = False
+
         print(f"Peca detectada! Total: {contador}")
 
     sleep_ms(20)
